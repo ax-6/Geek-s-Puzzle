d1b0e3dea4bb34c56bc3680e1920e5ca7de85c73
Xie Zicong <xie.zicong@axa6.com>
2025-02-08 10:20:21 +0800

    Added third-party.

--- main.cc
+++ main.cc
@@ -2,3 +2,10 @@
 // This program is licensed under GPL-3.0. You can find the GPL-3.0 in
 // the root directory.
 
+#include "main.h"
+
+int main() {
+  GeekSPuzzle::Main main;
+  main.Run();
+  return 0;
+}
--- main.h
+++ main.h
@@ -2,3 +2,18 @@
 // This program is licensed under GPL-3.0. You can find the GPL-3.0 in
 // the root directory.
 
+#include "third-party/wxWidgets-3.2.6/include/wx/wx.h"
+
+namespace GeekSPuzzle {
+class Main {
+ public:
+  Main() = default;
+  ~Main() = default;
+
+  void Run() {
+
+  }
+
+ private:
+};
+};  // namespace GeekSPuzzle
